74b48a4da7ee9277ee6f447794d744e834ad9bc2
vlangner <vlangner@usca.edu>
2022-11-07 13:12:51 -0500

    November 7

---
+++ js/add-element.js
@@ -0,0 +1,5 @@
+// test
+$('.hot').each(function(){
+    $(this).removeClass("hot");
+    $(this).addClass("cool");
+});
--- js/hotel.js
+++ js/hotel.js
@@ -15,14 +15,30 @@ console.log(test.numrooms);
 // event
 $(function(){
     $('#submit').click(reservation);
+    $('#lists').click(displayR);
 });
+
+var guestGroup = new Array();
 
 function reservation(){
     var guest = {
         guestName: $('#name').val(),
         checkIn: $('#checkin').val(),
         checkOut: $('#checkout').val(),
-        numberofGuests: $('#num').val(),
+        numberofGuests: $('#numguest').val(),
         roomType: $('#room').val(),
+    };
+
+    $("p:first").html("Thank you for your reservation.")
+    guestGroup.push(guest); // store in array
+    console.log(guestGroup);
+}
+
+function displayR(){
+    $("form").append("<table>");
+    for(var i in guestGroup){
+        var currentGuest = guestGroup[i];
+        var row = "<tr><td>" + currentGuest.guestName + "</tr></td>" + "<tr><td>" + currentGuest.checkIn + "</tr></td>";
+        $("table").append(row);
     }
 }
